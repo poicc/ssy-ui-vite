45f7f8a0aadf028e42fc8c9c2ecf5fadadecc283
陈蓉琪 <chenrongqi@njbandou.com>
2023-01-03 14:17:45 +0800

    项目初始化

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+// const s: string = 'Hello Typescript'
+// console.log(s)
+
+// import { createApp } from "vue";
+// import SButton from "./button";
+// createApp(SButton).mount("#app");
+
+// import { createApp } from "vue";
+// import SFCButton from "./SFCButton.vue";
+// createApp(SFCButton).mount("#app");
+
+import { createApp } from "vue";
+import JSXButton from "./JSXButton";
+
+createApp(JSXButton)
+  .mount("#app");
