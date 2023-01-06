fda791f61bad1c6972ee311601528a70d4e141df
陈蓉琪 <chenrongqi@njbandou.com>
2023-01-06 19:35:23 +0800

    feat(custom): 组件按需引入

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
-import Unocss from "./config/unocss";
+import UnoCss from "./config/unocss";
 import { UserConfig } from "vitest";
 
 const rollupOptions = {
@@ -14,44 +14,43 @@ const rollupOptions = {
   },
 };
 
-// https://vitejs.dev/config/
 export const config = {
   plugins: [
     vue(),
-    // 添加JSX插件
     vueJsx(),
-    // 添加UnoCSS插件
-    Unocss(),
+    UnoCss(),
   ],
-
-  //添加库模式配置
   build: {
     rollupOptions,
-    minify: "terser", // boolean | 'terser' | 'esbuild'
+    minify: `terser`, // boolean | 'terser' | 'esbuild'
     sourcemap: true, // 输出单独 source文件
     brotliSize: true, // 生成压缩大小报告
-    cssCodeSplit: true, // 在编译的时候是否要独立输出 css
     lib: {
       entry: "./src/entry.ts",
       name: "SSYUI",
       fileName: "ssy-ui",
-      // 导出模块格式
-      formats: ["esm", "umd", "iife"],
+      formats: ["esm", "umd", "iife"], // 导出模块类型
     },
     outDir: "./dist",
+    cssCodeSplit: true
   },
-
   test: {
     // enable jest-like global test APIs
     globals: true,
     // simulate DOM with happy-dom
     // (requires installing happy-dom as a peer dependency)
-    environment: "happy-dom",
+    environment: 'happy-dom',
     // 支持tsx组件，很关键
     transformMode: {
       web: [/.[tj]sx$/],
     },
+    coverage: {
+      provider: "istanbul", // or 'c8',
+      reporter: ["text", "json", "html"],
+    },
   },
+
 };
 
+// https://vitejs.dev/config/
 export default defineConfig(config as UserConfig);
