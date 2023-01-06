69cd3ef36b1b9b9dbace0cf09ad4b44bdb5b7a05
陈蓉琪 <chenrongqi@njbandou.com>
2023-01-06 20:56:35 +0800

    fix(custom): 修改button引用方式

--- src/entry.ts
+++ src/entry.ts
@@ -19,7 +19,7 @@
 // };
 
 import { App } from "vue";
-import Button from "./Button/Button";
+import Button = require("./Button");
 import "uno.css";
 
 // 导出单独组件
@@ -28,6 +28,6 @@ export { Button };
 // 编写一个插件，实现一个install方法
 export default {
   install(app: App): void {
-    app.component(Button.name, Button);
+    app.component(Button.Button.name, Button);
   },
 };
