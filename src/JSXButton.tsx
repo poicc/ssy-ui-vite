b808354d5df342eb5e7ab4dfbfecd703f7cd7c91
陈蓉琪 <chenrongqi@njbandou.com>
2023-01-07 10:06:54 +0800

    测试

--- src/JSXButton.tsx
+++
@@ -1,8 +0,0 @@
-// import { defineComponent, h } from "vue";
-
-// export default defineComponent({
-//   name: "JSXButton",
-//   setup() {
-//     return <button>JSX Button</button>;
-//   },
-// });
--- src/fetch.js
+++
@@ -1,2 +0,0 @@
-// const axios = require('axios')
-// exports.getData = () => axios.get('/abc/bcd')
